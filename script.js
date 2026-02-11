1578bebca99d2a133313f0d8ec08a1942798629f
Gaurav <183895839+Gaurav-zero@users.noreply.github.com>
2026-02-11 20:46:43 +0530

    add a button to form which adds the new book to library
    added event listener to add button in the form so that on clicking it,
    book gets added to library

--- script.js
+++ script.js
@@ -31,14 +31,55 @@ btn.addEventListener("click", (e) => {
     let l1= document.createElement("label");
     l1.setAttribute('for', "bookName");
     l1.textContent= "Book Name";
+    
 
     let i1= document.createElement("input");
     i1.setAttribute('type', "text");
     i1.setAttribute('name', "bookName");
     i1.setAttribute('id', "bookName");
+    
 
-    f.appendChild(i1);
+    let l2= document.createElement("label");
+    l2.setAttribute('for', "author");
+    l2.textContent= "Author";
+    
+
+    let i2= document.createElement("input");
+    i2.setAttribute('type', "text");
+    i2.setAttribute('name', "author");
+    i2.setAttribute('id', "author");
+
+    let l3= document.createElement("label");
+    l3.setAttribute('for', "pages");
+    l3.textContent= "No. of Pages";
+    
+
+    let i3= document.createElement("input");
+    i3.setAttribute('type', "text");
+    i3.setAttribute('name', "pages");
+    i3.setAttribute('id', "pages");
+
+    let formButton= document.createElement("button");
+    formButton.setAttribute('type', "submit");
+    formButton.textContent= "Add";
+
+    f.addEventListener("submit" , (e)=>{
+        e.preventDefault();
+        addBookToLib(i1.value, i2.value, i3.value);
+        i1.value="";
+        i2.value="";
+        i3.value="";
+    });
+
+
     f.appendChild(l1);
+    f.appendChild(i1);
+    f.appendChild(l2);
+    f.appendChild(i2);
+    f.appendChild(l3);
+    f.appendChild(i3);
+    f.appendChild(formButton);
+    
     body.appendChild(f);
 });
 
